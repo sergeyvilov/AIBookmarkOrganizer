5c1dd0b38b690213ecb5285f0d45d36d3462793e
sergeyvilov <vilovskimail@gmail.com>
2025-05-02 22:18:58 +0200

    adding installation scripts and instructions

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+npm install --save-dev webpack@5.99.6 webpack-cli
+npx webpack --config webpack.config.js
---
+++ prepare_extension.sh
@@ -0,0 +1,11 @@
+rm -r production/
+
+ext_dir=production/extension/
+src_dir=production/source/
+
+mkdir -p $ext_dir $src_dir
+
+cp -r README.MD src icons manifest.json options.html build.sh webpack.config.js $src_dir
+cp -r dist icons manifest.json options.html $ext_dir
+
+rm $ext_dir/dist/*.map
